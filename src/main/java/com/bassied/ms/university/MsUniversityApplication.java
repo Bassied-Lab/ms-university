7e4effb7d6f8666557694099d186aa1460ec3cf5
seynurmammadov <seynursm@code.edu.az>
2023-02-28 23:24:11 +0400

    Added skeleton

---
+++ src/main/java/com/bassied/ms/university/MsUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.bassied.ms.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUniversityApplication.class, args);
+	}
+
+}
