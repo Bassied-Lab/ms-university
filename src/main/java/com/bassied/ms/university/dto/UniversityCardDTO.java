76e821697a5877e3ed61542643745334b3f029d7
sabuhihuseynov <huseynovsebuhi14@gmail.com>
2023-03-06 12:18:43 +0400

    changes

--- src/main/java/com/bassied/ms/university/dto/UniversityCardDTO.java
+++ src/main/java/com/bassied/ms/university/dto/UniversityCardDTO.java
@@ -4,6 +4,6 @@ package com.bassied.ms.university.dto;
 import com.bassied.ms.university.enums.City;
 import com.bassied.ms.university.enums.Country;
 
-public record UniversityCardDTO(Long id, City city, Country country, String name, Integer facultyCount,
-                                Integer minPrice) {
+public record UniversityCardDTO(Long id, City city, Country country, String name, String logo,
+                                Integer facultyCount, Integer minPrice) {
 }
